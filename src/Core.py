308cb9e95f338c147e5b8889dd523f8ac258ab1a
LLLgoyour <lllgoyour@protonmail.com>
2023-11-10 13:04:13 -0700

    V5 Python API

---
+++ src/Core.py
@@ -0,0 +1,79 @@
+from vex import *
+import time
+
+# Stores each object's current position and associated functions
+# X = x position, Y = y position, W = width, H = height
+# F = associated callback function, S = signal required to run function (clicked, pressed, etc)
+objectsXYWHSF = []
+
+class V5GLError(Exception):
+    def __init__(self, message="Sorry, V5GL encountered an internal error."):
+        self.message = message
+        super().__init__(self.message)
+
+
+class V5Application:
+    # bg and background are different names for the same thing, both will produce desired result.
+    # Refresh rate determines the screen refresh rate in hertz
+    def __init__(self, bg="white", background="", refresh_rate=60):
+        self.LoopSpeed = None
+        self.background = bg
+        if not background == "":
+            self.background = background
+        self.refreshRate = refresh_rate
+
+        brain.screen.clear_screen()
+        brain.screen.set_fill_color(self, self.background)
+        brain.screen.set_pen_color(self, Color.TRANSPARENT)
+        brain.screen.draw_rectangle(0, 0, 479, 239)
+
+    def Run(self):
+        # This is the main loop of the GUI
+        self.lastPressX = None
+        self.lastPressY = None
+
+        while True:
+            self.pressX = brain.screen.x_position()
+            self.pressY = brain.screen.y_position()
+
+            # Test if the screen has been touched and if so, get the coordinates of the touch
+            if self.pressX != self.lastPressX or self.pressY != self.lastPressY:
+                self.lastPressX = self.pressX
+                self.lastPressY = self.pressY
+
+                for object in objectsXY:
+                    if self.pressX <= object[0] + object[2] and self.pressX >= object[0] and self.pressY <= object[1] + object[3] and self.pressY >= object[1]:
+                        object[4]()
+
+            # If the loop is running at a faster rate than the screen refresh rate, then wait until the next screen refresh
+            if self.LoopSpeed != None:
+                if self.LoopSpeed > self.refreshRate:
+                    time.sleep(self.LoopSpeed / self.refreshRate)
+                else:
+                    time.sleep(1 / self.refreshRate)
+            else:
+                time.sleep(1 / self.refreshRate)
+
+            self.lastPressX = pressX
+            self.lastPressY = pressY
+
+    def ClearScreen(self):
+        brain.screen.clear_screen()
+
+    def SetBackground(self, bg):
+        self.background = bg
+
+    def SetRefreshRate(self, rate):
+        self.refreshRate = rate
+
+    def GetBackground(self):
+        return self.background
+
+    def GetRefreshRate(self):
+        return self.refreshRate
+
+    def GetObjectsXY(self):
+        return objectsXY
+
+
+
---
+++ src/Widgets.py
@@ -0,0 +1,90 @@
+from vex import *
+
+# image is a list of lists of the average RGB values of each pixel in each column in the image
+def BoxBlur(image, radius):
+    # Blurs a greyscale image using a box filter.
+    if radius < 1:
+        return image
+    w = len(image)
+    h = len(image[0])
+    img = image
+    for x in range(w):
+        for y in range(h):
+            for i in range(radius):
+
+    for y in range(h):
+        for x in range(w):
+            r = 0
+            g = 0
+            b = 0
+            rn = 0
+            gn = 0
+            bn = 0
+            for i in range(max(0, y - radius), min(h, y + radius + 1)):
+                for j in range(max(0, x - radius), min(w, x + radius + 1)):
+                    p = img.get_pixel(j, i)
+                    r += p.red
+                    g += p.green
+                    b += p.blue
+                    rn += 1
+                    gn += 1
+                    bn += 1
+            img.set_pixel(x, y, Color(r / rn, g / gn, b / bn))
+    return img
+
+
+
+def draw_rect(xCoordinate, yCoordinate, width, height, borderRadius):
+    """Draws a rectangle of the specified color at the specified location."""
+    brain.screen.draw_rectangle(xCoordinate + borderRadius, yCoordinate + borderRadius,
+                                width - (borderRadius * 2), height - (borderRadius * 2))
+    brain.screen.draw_circle(xCoordinate - borderRadius, yCoordinate + borderRadius,
+                             borderRadius)
+    brain.screen.draw_circle(xCoordinate - borderRadius + width,
+                             yCoordinate + borderRadius, borderRadius)
+    brain.screen.draw_circle(xCoordinate - borderRadius + width,
+                             yCoordinate - borderRadius + height, borderRadius)
+    brain.screen.draw_circle(xCoordinate - borderRadius,
+                             yCoordinate + borderRadius + height, borderRadius)
+    brain.screen.draw_rectangle(xCoordinate + borderRadius, yCoordinate,
+                                width - (borderRadius * 2), borderRadius)
+    brain.screen.draw_rectangle(xCoordinate + borderRadius, yCoordinate,
+                                width - (borderRadius * 2), borderRadius)
+
+
+class PushButton:
+    def __init__(self, x_coordinate, y_coordinate, width, height, bg="#EEEEEE", background="", text="",
+                 drop_shadow=False, drop_shadow_x=-3, drop_shadow_y=3, drop_shadow_blur=3, drop_shadow_color="#d6d6d6",
+                 border_radius=0):
+        self.dropShadowX = drop_shadow_x
+        self.dropShadowY = drop_shadow_y
+        self.dropShadowBlur = drop_shadow_blur
+        self.dropShadowColor = drop_shadow_color
+        self.background = bg
+        if not background == "":
+            self.background = background
+        self.text = text
+        self.dropShadow = drop_shadow
+        self.xCoordinate = x_coordinate
+        self.yCoordinate = y_coordinate
+        self.width = width
+        self.height = height
+        self.borderRadius = border_radius
+        brain.screen.set_fill_color(self, self.background)
+        brain.screen.set_pen_color(self, Color.TRANSPARENT)
+
+        if self.dropShadow:
+            brain.screen.set_pen_color(self, self.dropShadowColor)
+            brain.screen.set_fill_color(self, self.dropShadowColor)
+            brain.screen.draw_rectangle(self.xCoordinate + self.dropShadowX,
+                                        self.yCoordinate + self.dropShadowY,
+                                        self.width, self.height)
+            brain.screen.set_pen_color(self, self.background)
+            brain.screen.set_fill_color(self, self.background)
+
+        if not self.borderRadius == 0:
+            draw_rect(self.xCoordinate, self.yCoordinate, self.width, self.height, self.borderRadius)
+
+        else:
+            brain.screen.set_fill_color(self, self.background)
+            brain.screen.draw_rectangle(self.xCoordinate, self.yCoordinate, self.width, self.height)
